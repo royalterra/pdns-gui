5eab494c8a680d27d957b91f9f8a378615718223
chris.maciejewski <chris.maciejewski@6b7caaca-a955-11de-8c46-7bd654735883>
2009-09-30 08:27:58 +0000

    ->getCreatedAt added

--- lib/model/Domain.php
+++ lib/model/Domain.php
@@ -9,6 +9,21 @@
  */ 
 class Domain extends BaseDomain
 {
+  /**
+   * Gets timestamp when domain was created
+   */
+  public function getCreatedAt()
+  {
+    $c = new Criteria();
+    $c->add(AuditPeer::OBJECT, 'Domain');
+    $c->add(AuditPeer::TYPE, 'ADD');
+    $c->add(AuditPeer::OBJECT_KEY, $this->getId());
+    $c->addAscendingOrderByColumn(AuditPeer::ID);
+    
+    $audit = AuditPeer::doSelectOne($c);
+    
+    return $audit->getCreatedAt();
+  }
 }
 
 sfPropelBehavior::add('Domain', array('audit'));
